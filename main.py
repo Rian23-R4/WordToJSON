7af57f871a2343a5119a66cdbc6e0f709bae6303
Rian <ryan23nugraha23@gmail.com>
2023-03-06 14:17:48 +0700

    fix regex artikel dan gambar

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import docx2txt
 
 data = []
 # regex untuk memisahkan pertanyaan, pilihan jawaban, dan jawaban yang benar
-regex = r'(\d+)\.?\s+(.*?)\s+(a)\.?\s+(.*?)\s+(b)\.?\s+(.*?)\s+(c)\.?\s+(.*?)\s+(d)\.?\s+(.*?)\s+(e)\.?\s+(.*?)\s+Jawaban\s+:\s+(.*?)\s+'
+regex = r'(\d+)\.\s+(.*?)\s+(a)\.\s+(.*?)\s+(b)\.\s+(.*?)\s+(c)\.\s+(.*?)\s+(d)\.\s+(.*?)\s+(e)\.\s+(.*?)\s+Jawaban\s+:\s+(.*?)\s+'
 
-regex_artikel = r'Artikel\s:\s+(.*?)\s+\d+\.'
+# regex untuk mengambil nomor artikel
+regex_artikel = r'Artikel\s+(.*?)\s+:\s+([\s\S]*?)\s?\n\d+\.'
+# Artikel\s(\d+)\s:\n(?:[\w\s.,-]+?\n)+?(?:\d+|$)
+
+# regex untuk mengambil nomor gambar
+regex_gambar = r'Gambar\s+(.*?)\s+:\s+'
 
 # membuka file yang berisi soal-soal
 # text = docx2txt.process("Soal PPS Bagian 1.docx")
@@ -42,4 +47,35 @@ for match in matches:
 
 # menyimpan data soal dalam format json ke dalam file
 with open("soal.json", "w") as f:
+    json.dump(data, f)
+
+
+# mencari semua data artikel menggunakan regex
+matches = re.findall(regex_artikel, text, re.DOTALL)
+
+# mengonversi data artikel menjadi format json
+data = []
+for match in matches:
+    data.append({
+        "number": match[0],
+        "artikel": match[1]
+    })
+
+# menyimpan data artikel dalam format json ke dalam file
+with open("artikel.json", "w") as f:
+    json.dump(data, f)
+
+# mencari semua data gambar menggunakan regex
+matches = re.findall(regex_gambar, text, re.DOTALL)
+
+# mengonversi data gambar menjadi format json
+data = []
+for index, match in enumerate(matches):
+    data.append({
+        "number": match,
+        "image": f"image{index+1}.png"
+    })
+
+# menyimpan data gambar dalam format json ke dalam file
+with open("gambar.json", "w") as f:
     json.dump(data, f)
